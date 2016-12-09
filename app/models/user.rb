d736d591145ec6f21a06d3ef977fed7c6f8b85bf
Nguyen The Toan <thetoan95@gmail.com>
2016-12-09 13:15:22 +0700

    User sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   has_many :active_relationships,  class_name: Relationship.name,
     foreign_key: :follower_id, dependent: :destroy
   has_many :passive_relationships, class_name: Relationship.name,
@@ -11,4 +14,14 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :activities, as: :activatable, dependent: :destroy
   has_many :likes, as: :likable, dependent: :destroy
+
+  before_save {self.email = email.downcase}
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true,
+    length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}, allow_nil: true
+  validates :password, length: {minimum: 6}, allow_nil: true
+  has_secure_password
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get "contact", to: "static_pages#contact"
+  resources :users
+  get "signup" => "users#new"
+  post "signup" => "users#create"
 end
