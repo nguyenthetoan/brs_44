5babd596c4406861c6454bea032d4daa9de29b7b
Nguyen The Toan <thetoan95@gmail.com>
2016-12-13 10:14:28 +0700

    Admin update book

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -50,23 +50,101 @@ $(document).on('turbolinks:load', function() {
     active4 = !active4;
   });
 
-  $modal = $('#modal-new-book');
-
-  $btn = $('#btn-new-book');
-
-  $span = $('.close');
-
-  $btn.click(function() {
-    $modal.css('display', 'block')
-  })
-
-  $span.click(function() {
-    $modal.fadeOut();
+  $('.close').click(function() {
+    $('#modal-new-book').fadeOut();
+    $('#modal-edit-book').fadeOut();
   })
 
   $('body').click(function(event) {
-    if (event.target.id == $modal.attr('id')) {
-      $modal.fadeOut();
+    if (event.target.id == $('#modal-new-book').attr('id')) {
+      $('#modal-new-book').fadeOut();
+      $('#modal-edit-book').fadeOut();
     }
   })
-});
+
+  $('#btn-new-book').click(function(e) {
+    e.preventDefault()
+    $('#modal-new-book').css('display', 'block')
+    $.ajax({
+      dataType: "html",
+      url: "books/new",
+      method: "get",
+      success: function(data) {
+        $('.modal-body').html(data)
+      }
+    });
+    return false
+  })
+
+  $('body').on('click', '#new_book', function() {
+    $form = $(this);
+    $form.submit(function(e) {
+      e.preventDefault();
+      e.stopImmediatePropagation();
+      $.ajax({
+        url: $form.attr('action'),
+        method: $form.attr('method'),
+        dataType: "html",
+        data: $form.serialize(),
+        success: function(data) {
+          $('#modal-new-book').fadeOut();
+          $('tbody').append(data).children(':last').hide().fadeIn(1000)
+        }
+      })
+    })
+  });
+
+  $('body').on('click', '.edit_book', function() {
+    $form = $(this);
+    $form.submit(function(e) {
+      e.preventDefault();
+      e.stopImmediatePropagation();
+      $.ajax({
+        url: $form.attr('action'),
+        method: $form.attr('method'),
+        dataType: "html",
+        data: $form.serialize(),
+        success: function(data) {
+          $('#modal-edit-book').fadeOut();
+          $('tbody').html(data).hide().fadeIn(1000)
+        }
+      })
+    })
+  });
+
+  $('body').on('click', '.btn-edit-book', function(e) {
+    e.preventDefault();
+    $('#modal-edit-book').css('display', 'block')
+    $url = $(this).attr('href')
+    console.log($url)
+    $.ajax({
+      dataType: "html",
+      url: $url,
+      method: $(this).attr('method'),
+      success: function(data) {
+        $('.edit-modal-body').html(data)
+      }
+    });
+    return false
+  })
+
+  $('body').on('click', '.btn-delete', function(e) {
+    e.preventDefault();
+    if (confirm("Are you sure?")) {
+      $book_id = $(this).attr('href').split('/')[3]
+      $tr_id = '#book_' + $book_id
+      console.log($tr_id)
+      $.ajax({
+        dataType: "html",
+        url: $(this).attr('href'),
+        method: "DELETE",
+        success: function() {
+          $($tr_id).fadeOut();
+        }
+      })
+    } else {
+
+    }
+    return false;
+  })
+})
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
 
   belongs_to :category
 
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true
   validates :publish_date, presence: true
   validates :author, presence: true
 end
